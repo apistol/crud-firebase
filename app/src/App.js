00ff99eca482abbe731c93324d9b192f0a661e18
ashcsh <cvictorpodgoreanu@gmail.com>
2022-08-17 19:17:44 +0300

    Versiunea 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
+
+//MUIe
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+
 
 function App() {
 
-  const [users, setUsers] = useState([])
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
 
-  useEffect( () => {
-    axios.get("http://localhost:5000/ecommerce-2ebae/us-central1/api/users").then(res => {
-      setUsers(res.data.result)
+  const handleChange = () => {
+    debugger
+    axios.post("http://localhost:5000/ecommerce-2ebae/us-central1/api/users", {email, password})
+    .then((res) => {
+      console.log(res)
     })
-  }, [])
-
+    .catch((err) => {
+      console.error(err)
+    })
+  
+  }
 
   return (
-    <div>
-      Test ashu
-        {users.map( user => <p key={user.email}>{user.name} | {user.email}</p>)}
+    <div style={{height: "100vh"}}>
+      <Grid
+  container
+  direction="row"
+  justifyContent="center"
+  alignItems="center"
+  height="100%"
+>
+  <Grid item xs={4}>
+    <Paper elevation={4} style={{display:"flex", flexDirection:"column", justifyContent:"center", padding:"30px"}}>
+      <Typography textAlign="center" marginBottom="30px" variant="h3" componenet="h3">
+      Login
+      </Typography>
+      <TextField onChange={(e) => {setEmail(e.target.value)}}id="outlined-basic" label="Username" variant="outlined" />
+      <TextField onChange={(e) => {setPassword(e.target.value)}}style={{marginTop:25, marginBottom:25}} id="outlined-basic" label="Password" variant="outlined" />
+      <Button onClick={handleChange} style={{width: 100, margin: "auto"}} variant="contained">Login</Button>
+    </Paper>
+  </Grid>
+</Grid>
     </div>
   );
 }
