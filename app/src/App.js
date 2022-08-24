d70ac0a65b0a3b6d034e8eb794edfffbf1e3ff3a
EXT_API97 <EXT_API97@open-groupe.com>
2022-08-24 12:19:02 +0300

    tema razvan

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
 
-  const [users, setUsers] = useState([])
 
-  useEffect( () => {
-    axios.get("http://localhost:5000/ecommerce-2ebae/us-central1/api/users").then(res => {
-      setUsers(res.data.result)
-    })
-  }, [])
+  // [
+  //   {
+  //     id: 1,
+  //     task: "Learn HTML and CSS"
+  //   },
+  //   {
+  //     id: 2,
+  //     task: "Learn Javascript"
+  //   },
+  //   {
+  //     id: 3,
+  //     task: "Learn React"
+  //   }
+  // ]
 
+
+  //  state pentru todos ( tine minte ca trebuie sa fie array [])
+  //  state pentru new todo ( trebuie sa fie string )
 
   return (
     <div>
-      Test pistol
-        {users.map( user => <p key={user.email}>{user.name} | {user.email}</p>)}
+
+      {/* Itereaza prin todos, folosind map  */}
+
+
+      {/* Create a new todo  */}
+
+      {/* Input, care comunica bidirectional cu state ul sau */}
+      {/* Un button care face handle add todo 
+          actualizezi state ul cu un array nou,
+          spread-uiesti state ul vechi
+          si adaugi la sfarsitul listei, todo-ul nou
+      */}
     </div>
   );
 }
