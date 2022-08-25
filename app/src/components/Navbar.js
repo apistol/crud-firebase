2aa2c67fa45b75e95d8b64a66af8cca2de5db422
MusatRobert <musatrobert18@stud.ase.ro>
2022-08-25 17:42:07 +0300

    UPDATE

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,2 +1,9 @@
 // foloseste Link din react-router-dom
 // si doar afiseaza rutele posibile
+import React from 'react'
+
+ export default function Navabr() {
+  return (
+    <div>Navbar</div>
+  )
+}
